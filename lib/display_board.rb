fc616d333a180b81c58240bfde957b3abc3ffe45
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-15 17:14:04 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -20,4 +20,14 @@ def display_board(board)
   puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 
-display_board(board)
+display_board(board1)
+display_board(board2)
+display_board(board3)
+display_board(board4)
+display_board(board5)
+display_board(board6)
+display_board(board7)
+display_board(board8)
+display_board(board9)
+display_board(board10)
+display_board(board11)
